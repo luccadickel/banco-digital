72f7672f0bb4d3c453538952d9ae15ead315cc5b
luccadickel <luccadickel@hotmail.com>
2026-09-03 09:07:32 -0300

    feat: incluindo inserts sql

---
+++ data/inserts.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (name) VALUES ('João da Silva');
+INSERT INTO users (name) VALUES ('Maria Alves');
+INSERT INTO users (name) VALUES ('José Pereira');
+INSERT INTO users (name) VALUES ('Carlos Eduardo');
+INSERT INTO users (name) VALUES ('Cristian Oliveira');
+INSERT INTO users (name) VALUES ('Pedro Paiva');
+INSERT INTO users (name) VALUES ('Guilherme Silva');
+INSERT INTO users (name) VALUES ('Maria Eduarda');
+INSERT INTO users (name) VALUES ('Rafael Silva');
+INSERT INTO users (name) VALUES ('Gabriel Abreu');
+
+
+INSERT INTO accounts (user_id, balance) VALUES (1, 1000.00);
+INSERT INTO accounts (user_id, balance) VALUES (2, 500.00);
+INSERT INTO accounts (user_id, balance) VALUES (3, 250.00);
+INSERT INTO accounts (user_id, balance) VALUES (4, 0.00);
+INSERT INTO accounts (user_id, balance) VALUES (5, 300.00);
+INSERT INTO accounts (user_id, balance) VALUES (6, 200.00);
+INSERT INTO accounts (user_id, balance) VALUES (7, 2000.00);
+INSERT INTO accounts (user_id, balance) VALUES (8, 800.00);
+INSERT INTO accounts (user_id, balance) VALUES (9, 750.00);
+INSERT INTO accounts (user_id, balance) VALUES (10, 300.00);
+
